ab573f9dc4b4a17951366705ceaac1f6bff41f12
Stefan <p.c.s.vermeent@uu.nl>
2022-12-09 15:40:36 +0100

    update to repo dependencies

--- scripts/dependencies.R
+++ scripts/dependencies.R
@@ -8,25 +8,30 @@ pkgs <-
     "gert",
     "glue",
     "stringdist",
-    "rjags",
-    "coda",
-    "rstan",
-    "hBayesDM",
+    "runjags",
     "htmltools",
-    "gt",
     "flextable",
     "lavaan", 
-    "lavaan.survey"
+    "lavaan.survey",
+    "assertthat",
+    "english"
   )
 
 # The date of which we want to install and load the package versions
 date <- "2022-10-15"
 
 # Using the groundhog package, we install and load the project dependencies
-groundhog.library(pkg = pkgs, date = date, force.install = TRUE)
+groundhog.library(pkg = pkgs, date = date)
 
 
 # Data folder
-# Set this to `synthetic_data` if you want to reproduce the analyses
+# Set this to data if you want to reproduce the analyses
 # using the synthetic data files.
 data_folder <- 'closed_data'
+
+
+# Source functions
+source('scripts/custom_functions/read-functions.R')
+source('scripts/custom_functions/general-functions.R')
+
+
