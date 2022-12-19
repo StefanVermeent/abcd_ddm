24ad03ff9796e26aba0cf3da12c1e72f4da61aa4
Stefan <p.c.s.vermeent@uu.nl>
2022-12-19 09:29:15 +0100

    updated supplement

--- supplement/scripts/staging_suppl.R
+++ supplement/scripts/staging_suppl.R
@@ -1,10 +1,11 @@
 
 
 # Load objects ------------------------------------------------------------
-load('synthetic_data/power.RData')
-load('synthetic_data/sim_DDM_6trials.RData')
-load('synthetic_data/ddm_sim1_results.RData')
-load('synthetic_data/ddm_sim2_results.RData')
+load('analysis_objects/tasks_clean.RData')
+load('analysis_objects/power.RData')
+load('analysis_objects/ddm_sim1_results.RData')
+load('analysis_objects/ddm_sim2_results.RData')
+load('analysis_objects/ddm_sim3_results.RData')
 
 # Power analysis ----------------------------------------------------------
 
@@ -41,7 +42,7 @@ conv_sim1_data <- ddm_sim1_traces |>
   ) |> 
   pivot_longer(-c(n, chains), names_to = 'parameter', values_to = 'value')
 
-traces_plot_sim2 <- conv_sim2_data |> 
+traces_plot_sim1 <- conv_sim1_data |> 
   ggplot(aes(n, value, color = factor(chains))) +
   geom_line() +
   facet_wrap(~parameter, scales = 'free') +
@@ -53,7 +54,7 @@ traces_plot_sim2 <- conv_sim2_data |>
     color = "Chain"
   )
 
-distr_plot_sim2 <- conv_sim2_data |> 
+distr_plot_sim1 <- conv_sim1_data |> 
   ggplot(aes(value, fill = parameter)) +
   geom_histogram() +
   facet_wrap(~parameter, scales = 'free') +
@@ -66,7 +67,25 @@ distr_plot_sim2 <- conv_sim2_data |>
   ) +
   guides(fill = 'none')
 
+## Parameter Recovery ----
 
+recov_plot_sim1 <- ddm_sim1_data |> 
+  ggplot(aes(simulated, estimated)) +
+  geom_point() +
+  geom_abline(slope = 1, intercept = 0) +
+  facet_wrap(~parameter, scales = 'free') +
+  scale_color_uchicago() +
+  theme_classic() +
+  labs(
+    x = "Simulated",
+    y = "Recovered"
+  )
+
+ddm_sim1_cor <- ddm_sim1_cor |> 
+  mutate(
+    r = as.character(round(r,2)) |> str_remove(string = _, "^0")) |> 
+  pivot_wider(names_from = "parameter", values_from = "r") |> 
+  as.list()
 
 
 # Simulation 2 ------------------------------------------------------------
@@ -114,7 +133,7 @@ distr_plot_sim2 <- conv_sim2_data |>
 recov_plot_sim2 <- ddm_sim2_data |> 
   mutate(condition = ifelse(is.na(condition), 'fixed', condition)) |> 
   ggplot(aes(simulated, estimated, color = condition)) +
-  geom_point(shape = 1) +
+  geom_point() +
   geom_abline(slope = 1, intercept = 0) +
   facet_wrap(~parameter, scales = 'free') +
   scale_color_uchicago() +
@@ -132,24 +151,133 @@ ddm_sim2_cor <- ddm_sim2_cor |>
   as.list()
 
 
+
+# Simulation 3 ------------------------------------------------------------
+
+## Convergence ----
+
+conv_sim3_data <- ddm_sim3_traces |> 
+  rename(
+    a_fixed = muAlpha,
+    t0_1    = `muTau[1]`, 
+    t0_2    = `muTau[2]`,
+    v_1     = `muDelta[1]`,
+    v_2     = `muDelta[2]`
+  ) |> 
+  pivot_longer(-c(n, chains), names_to = 'parameter', values_to = 'value') |> 
+  separate(parameter, into = c('parameter', 'condition'), sep = "_")
+
+traces_plot_sim3 <- conv_sim3_data |> 
+  unite(col = "parameter", parameter,condition, sep = ", condition ") |> 
+  ggplot(aes(n, value, color = factor(chains))) +
+  geom_line() +
+  facet_wrap(~parameter, scales = 'free') +
+  theme_classic() +
+  scale_color_uchicago() +
+  labs(
+    x = "",
+    y = "",
+    color = "Chain"
+  )
+
+distr_plot_sim3 <- conv_sim3_data |> 
+  ggplot(aes(value, group = condition, fill = condition)) +
+  geom_histogram() +
+  facet_wrap(~parameter, scales = 'free') +
+  theme_classic() +
+  scale_fill_uchicago() +
+  labs(
+    x = "",
+    y = "",
+    fill = "Condition"
+  ) 
+
+## Parameter Recovery ----
+
+recov_plot_sim3 <- ddm_sim3_data |> 
+  mutate(condition = ifelse(is.na(condition), 'fixed', condition)) |> 
+  ggplot(aes(simulated, estimated, color = condition)) +
+  geom_point() +
+  geom_abline(slope = 1, intercept = 0) +
+  facet_wrap(~parameter, scales = 'free') +
+  scale_color_uchicago() +
+  theme_classic() +
+  labs(
+    x = "Simulated",
+    y = "Recovered",
+    color = "Condition"
+  )
+
+ddm_sim3_cor <- ddm_sim3_cor |> 
+  mutate(
+    r = as.character(round(r,2)) |> str_remove(string = _, "^0")) |> 
+  pivot_wider(names_from = "parameter", values_from = "r") |> 
+  as.list()
+
+# Descriptives ------------------------------------------------------------
+
+descriptives <- list()
+
+descriptives$task_descriptives_table <- 
+  list(pcps_clean, flanker_clean, lmt_clean, dccs_clean) |> 
+  map_df(function(x) {
+    
+    x |> 
+      group_by(subj_idx, task) |> 
+      summarise(
+        rt = mean(RT, na.rm = T),
+        acc     = sum(correct)/n() * 100,
+      ) |> 
+      group_by(task) |> 
+      summarise(
+        across(
+          c(rt, acc),
+          list(mean = mean, sd = sd, min = min, max = max)
+        ))
+  }) |> 
+  select(-c(rt_min, rt_max)) |> 
+  mutate(
+    task = case_when(
+      task == 1 ~ "Mental Rotation",
+      task == 2 ~ "Flanker",
+      task == 3 ~ "Attention Shifting",
+      task == 4 ~ "Processing Speed"
+    ),
+    mean_rt = str_c(round(rt_mean, 2), " (", round(rt_sd, 2), ")"),
+    mean_acc = str_c(round(acc_mean, 2), " (", round(acc_sd, 2), ")"),
+    acc_min  = as.character(round(acc_min,2)),
+    acc_max  = as.character(round(acc_max,2))
+  ) |> 
+  select(task, mean_rt, mean_acc, acc_min, acc_max)
+
+
+
 # Stage all results -------------------------------------------------------
 
 staged_sim_results_supp <- 
   list(
     power_plot = power_plot,
     
-   # ddm_sim1_convergence = conv_sim1_data,
-   # ddm_sim1_traces_plot = traces_plot_sim1,
-   # ddm_sim1_distr_plot  = distr_plot_sim1,
-   # ddm_sim1_cor         = ddm_sim1_cor,
-   # ddm_sim1_recov       = recov_plot_sim2,
+    ddm_sim1_convergence = conv_sim1_data,
+    ddm_sim1_traces_plot = traces_plot_sim1,
+    ddm_sim1_distr_plot  = distr_plot_sim1,
+    ddm_sim1_cor         = ddm_sim1_cor,
+    ddm_sim1_recov       = recov_plot_sim1,
     
     ddm_sim2_convergence = conv_sim2_data,
     ddm_sim2_traces_plot = traces_plot_sim2,
     ddm_sim2_distr_plot  = distr_plot_sim2,
     ddm_sim2_cor         = ddm_sim2_cor,
-    ddm_sim2_recov       = recov_plot_sim2
+    ddm_sim2_recov       = recov_plot_sim2,
+    
+    ddm_sim3_convergence = conv_sim3_data,
+    ddm_sim3_traces_plot = traces_plot_sim3,
+    ddm_sim3_distr_plot  = distr_plot_sim3,
+    ddm_sim3_cor         = ddm_sim3_cor,
+    ddm_sim3_recov       = recov_plot_sim3,
+    
+    descriptives         = descriptives$task_descriptives_table
   )
 
-save(staged_sim_results_supp, file = 'synthetic_data/staged_sim_results_supp.RData')
+save(staged_sim_results_supp, file = 'supplement/staged_sim_results_supp.RData')
 
