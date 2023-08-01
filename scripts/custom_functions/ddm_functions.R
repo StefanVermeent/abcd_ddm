35a5b2a9a13990a0ad5f1328e62de39cbdb75d82
Stefan <p.c.s.vermeent@gmail.com>
2023-08-01 13:49:17 +0200

    DDM helper functions

---
+++ scripts/custom_functions/ddm_functions.R
@@ -0,0 +1,115 @@
+
+extract_traces <- function(mcmc, chains = 3, iterations = 12000) {
+  
+  mcmc |> 
+    select(starts_with("mu")) |> 
+    mutate(
+      n = rep(1:iterations, chains),
+      chains = rep(1:chains, each = iterations))  |> 
+    rename(
+      a  = muAlpha,
+      t0 = muTau, 
+      v  = muDelta
+    ) |> 
+    pivot_longer(-c(n, chains), names_to = 'parameter', values_to = 'value')
+}
+
+
+
+extract_traces_2con <- function(mcmc, chains = 3, iterations = 1000) {
+  
+  mcmc |> 
+    select(starts_with("mu")) |> 
+    mutate(
+      n = rep(1:iterations, chains),
+      chains = rep(1:chains, each = iterations))  |> 
+    rename(
+      a   = muAlpha,
+      t01 = `muTau[1]`, 
+      t02 = `muTau[2]`,
+      v1  = `muDelta[1]`,
+      v2  = `muDelta[2]`
+    ) |> 
+    pivot_longer(-c(n, chains), names_to = 'parameter', values_to = 'value')
+}
+
+
+extract_ddm_estimates <- function(mcmc, task_prefix, id_matches) {
+  
+  mcmc_lmt_mod1 |> 
+    pivot_longer(everything(), names_to = "parameter", values_to = "estimated") |> 
+    group_by(parameter) |> 
+    summarise(estimated = mean(estimated, na.rm = T)) |> 
+    filter(str_detect(parameter, pattern = 'deviance|^mu|^prec', negate = T)) |> 
+    separate(col = parameter, into = c('parameter', 'subj_idx_num'), sep = "\\[") |> 
+    mutate(
+      subj_idx_num = str_remove(subj_idx_num, pattern = "\\]$")
+    ) |>  
+    mutate(
+      subj_idx_num = as.numeric(subj_idx_num),
+      parameter = case_when(
+        parameter == 'alpha' ~ paste0(task_prefix, 'a'),
+        parameter == 'tau' ~ paste0(task_prefix, 't'),
+        parameter == 'delta' ~ paste0(task_prefix, 'v')
+      )) |> 
+    pivot_wider(names_from = "parameter", values_from = "estimated") |> 
+    left_join(id_matches) |> 
+    select(-subj_idx_num)
+}
+
+extract_ddm_estimates_2con <- function(mcmc, task_prefix, id_matches) {
+  
+  x <- mcmc |> 
+    pivot_longer(everything(), names_to = "parameter", values_to = "estimated") |> 
+    group_by(parameter) |> 
+    summarise(estimated = mean(estimated, na.rm = T)) |> 
+    filter(str_detect(parameter, pattern = 'deviance|^mu|^prec', negate = T)) |> 
+    separate(col = parameter, into = c('parameter', 'subj_idx_num'), sep = "\\[") |> 
+    mutate(
+      subj_idx_num = str_remove(subj_idx_num, pattern = "\\]$"),
+      subj_idx_num = ifelse(parameter %in% c('delta', 'tau'),
+             str_replace_all(subj_idx_num, "([0-9]*),([0-9]*)", "\\2,\\1"),
+             subj_idx_num
+      )
+    ) |> 
+    separate(subj_idx_num, into = c('subj_idx_num', 'condition')) |> 
+    mutate(
+      subj_idx_num = as.numeric(subj_idx_num),
+      parameter = case_when(
+        parameter == 'alpha' ~ paste0(task_prefix, 'a'),
+        parameter == 'tau' & condition == 1 ~ paste0(task_prefix, 't1'),
+        parameter == 'tau' & condition == 2 ~ paste0(task_prefix, 't2'),
+        parameter == 'delta' & condition == 1 ~ paste0(task_prefix, 'v1'),
+        parameter == 'delta' & condition == 2 ~ paste0(task_prefix, 'v2')
+      )) |> 
+    select(-condition) |> 
+    pivot_wider(names_from = "parameter", values_from = "estimated") |> 
+    left_join(id_matches) |> 
+    select(-subj_idx_num)
+}
+
+
+  mutate(
+    id = str_remove(id, pattern = "\\]$"),
+    id = ifelse(parameter %in% c('delta', 'tau'),
+                str_replace_all(id, "([0-9]*),([0-9]*)", "\\2,\\1"),
+                id
+    )
+  ) |> 
+  separate(id, into = c('id', 'condition')) |> 
+  mutate(
+    id = as.numeric(id),
+    parameter = case_when(
+      parameter == 'alpha' ~ 'a',
+      parameter == 'tau' ~ 't',
+      parameter == 'delta' ~ 'v'
+    )) |> 
+  left_join(
+    sim_parms3 |> 
+      select(id, starts_with('v_'), 'a_sim', starts_with('t_')) |> 
+      pivot_longer(-id, names_to = 'parameter', values_to = 'simulated') |> 
+      mutate(parameter = str_remove_all(parameter, "_sim$")) |> 
+      separate(parameter, into = c('parameter', 'condition'), sep = "_") |> 
+      mutate(condition = str_remove_all(condition, "con"))
+  )
+
