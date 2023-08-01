4eb80ac3113a8ad8e92a61a9ece3448502b44baf
Stefan <p.c.s.vermeent@gmail.com>
2023-08-01 13:45:20 +0200

    script to preprocess IVs

---
+++ scripts/2_analyses/2_ivs.R
@@ -0,0 +1,22 @@
+training_set  <- readr::read_csv(paste0("data/training_set", data_suffix, ".csv"))
+test_set  <- readr::read_csv(paste0("data/test_set", data_suffix, ".csv"))
+
+mnlfa_ivs <- read_csv("data/mnlfa_ivs.csv") |> 
+  select(subj_idx  = SUBID, 
+         dep_mnlfa = DepETA, 
+         threat_mnlfa = ThreatETA,                
+         age_c        = AGEC, 
+         sex          = FEMALE, 
+         age_m        = agemo,
+         eth_white    = WHITE, 
+         eth_black    = BLACK, 
+         eth_hisp     = HISPANIC, 
+         eth_other    = OTHER, 
+         inr          = INR, 
+         inr_c        = INRC,
+         high_edu     = HIGHED, 
+         high_edu_c   = HIGHEDC
+  )
+
+
+write_csv(mnlfa_ivs, "data/mnlfa_ivs_clean.csv")
